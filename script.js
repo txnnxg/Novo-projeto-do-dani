663d5d9097335c3aa0cfcd152f5e024536b4e772
txnnxg <143152390+txnnxg@users.noreply.github.com>
2026-01-16 21:36:11 -0300

    Deixando o site responsivo

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Carrossel automático simples (CSS já resolve)
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+
+// Fecha o menu ao clicar em qualquer item da lista (incluindo o X)
+document.querySelectorAll(".menu li").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+}));
