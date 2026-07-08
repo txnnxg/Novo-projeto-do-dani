105b156c810c8b633296b318e1f37a94e2c436cb
txnnxg <143152390+txnnxg@users.noreply.github.com>
2026-07-07 23:48:59 -0300

    Feito as fotos para mobile

--- script.js
+++ script.js
@@ -36,18 +36,22 @@ const fotosSemanais = [
     { 
         // Lembre-se de criar a pasta "semanais" e colocar as fotos com esses nomes
         fundo: 'imagens/semanais/bg-1.jpeg', 
+        fundoMobile: 'imagens/semanais/bg-1-mobile.jpeg',
         destaque: 'imagens/semanais/destaque-1.jpeg' 
     },
     { 
         fundo: 'imagens/semanais/bg-2.jpeg', 
+        fundoMobile: 'imagens/semanais/bg-2-mobile.png',
         destaque: 'imagens/semanais/destaque-2.jpeg' 
     },
     { 
         fundo: 'imagens/semanais/bg-3.jpeg', 
+        fundoMobile: 'imagens/semanais/bg-3-mobile.jpeg',
         destaque: 'imagens/semanais/destaque-3.jpeg' 
     },
     { 
         fundo: 'imagens/semanais/bg-4.png', 
+        fundoMobile: 'imagens/semanais/bg-4-mobile.jpeg',
         destaque: 'imagens/semanais/destaque-4.png' 
     }
 ];
@@ -65,19 +69,34 @@ function atualizarFotosDaSemana() {
         destaqueHero.src = fotosEscolhidas.destaque;
     }
 
-    // Atualiza o Fundo com o Degradê + a Foto Automática
+    // ==========================================
+    // A MÁGICA DO CELULAR ACONTECE AQUI
+    // ==========================================
+    const larguraTela = window.innerWidth;
+    let imagemFundoDaVez = fotosEscolhidas.fundo; // Por padrão, usa a de PC
+
+    if (larguraTela <= 768) {
+        imagemFundoDaVez = fotosEscolhidas.fundoMobile; // Se a tela for de celular, troca a foto!
+    }
+
+    // Atualiza o Fundo com o Degradê + a Foto Certa
     const fundoHero = document.querySelector('.hero'); 
     if (fundoHero) {
         fundoHero.style.backgroundImage = `
-                linear-gradient(to bottom,
-                rgba(0, 0, 0, 0.2) 0%,   /* Quase transparente no topo (mostra a foto) */
-                rgba(0, 0, 0, 0.8) 100%), /* Escuro embaixo (para ler o texto branco) */
-            url('${fotosEscolhidas.fundo}')
+            linear-gradient(to bottom,
+                rgba(0, 0, 0, 0.2) 0%,   
+                rgba(0, 0, 0, 0.8) 100%), 
+            url('${imagemFundoDaVez}')
         `;
     }
 }
 
 // Executa a troca de fotos assim que o site terminar de carregar
 document.addEventListener("DOMContentLoaded", () => {
+    atualizarFotosDaSemana();
+});
+
+// Executa a troca se a pessoa redimensionar a tela do navegador
+window.addEventListener("resize", () => {
     atualizarFotosDaSemana();
 });
